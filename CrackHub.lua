8dc81821d4f480f7dfc69a97ce16369dfb8de1a7
MihaMacroCreator <121865898+MihaMacroCreator@users.noreply.github.com>
2023-03-03 17:35:39 +0200

    Gaga

--- CrackHub.lua
+++ CrackHub.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet(("https://pastebin.com/raw/g41YF3YD"),true))()
 loadstring(game:HttpGet(('https://pastebin.com/raw/PVytRktS'),true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "CrackHub | Orion", HidePremium = false, SaveConfig = true, ConfigFolder = "cracked hub orion", IntroEnabled = true, IntroText = "v1.8", Icon = 7074749})
+local Window = OrionLib:MakeWindow({Name = "CrackHub | Orion", HidePremium = false, SaveConfig = true, ConfigFolder = "Miha having sex with Jumanji Full Porn Video Watch for Free", IntroEnabled = true, IntroText = "v1.8", Icon = 7074749})
 OrionLib:MakeNotification({
 	Name = "CrackHub",
 	Content = "Loading...",
